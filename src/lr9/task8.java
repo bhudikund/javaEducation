3f44d63416ca4289a470c6e97f531b742cc042b8
bhudikund <schooolmann@gmail.com>
2024-03-15 04:55:58 +0500

    upd

--- src/lr9/task8.java
+++ src/lr9/task8.java
@@ -1,6 +1,7 @@
 package lr9;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class task8 {
     static class Node{
@@ -10,10 +11,18 @@ public class task8 {
             this.value = value;
             this.next = next;
         }
+    static class Rec{
+            int value;
+            Rec next;
+            Rec(int value){
+                this.value= value;
+                this.next= null;
+            }
 
 
         static class Cycle {
             public Node head;
+            public Rec headR;
 
 
             public Node CreateHead() {
@@ -26,7 +35,7 @@ public class task8 {
 
 
             public Node CreateTail() {
-                Node head = null;
+                head = null;
                 for (int i = 9; i >= 0; i--) {
                     head = new Node(i, head);
                 }
@@ -39,12 +48,12 @@ public class task8 {
             }
 
             public Node addLast(int value){
-                Node newtail = new Node(value, null);
+                Node newTail = new Node(value, null);
                 Node ref = head;
                 while (ref.next != null) {
                     ref = ref.next;
                 }
-                ref.next=newtail;
+                ref.next=newTail;
                 return ref;
             }
 
@@ -92,18 +101,21 @@ public class task8 {
                 return ref;
             }
 
-            public String getNode(){
-                Node getNode = head;
-                StringBuilder sb = new StringBuilder();
-                while (getNode != null) {
-                    sb.append(getNode.value).append(" ");
-                    getNode = getNode.next;
-                }
-                return sb.toString();
+            public Node getNode(){
+                return head;
             }
 
             public String toStringHead() {
                 Node stringOut = CreateHead();
+                StringBuilder sb = new StringBuilder();
+                while (stringOut != null) {
+                    sb.append(stringOut.value).append(" ");
+                    stringOut = stringOut.next;
+                }
+                return sb.toString();
+            }
+            public static String toStringTest(Node test) {
+                Node stringOut = test;
                 StringBuilder sb = new StringBuilder();
                 while (stringOut != null) {
                     sb.append(stringOut.value).append(" ");
@@ -120,26 +132,125 @@ public class task8 {
                 }
                 return sb.toString();
             }
+                /*
+                public void CreateHeadR(int counter){
+                 head = CreateHeadRec(counter,head);
+                }
+                public Node CreateHeadRec(int counter, Node newNode){
+                    if (newNode == null){
+                        return new Node(counter, head);
+                    }
+                        Node newNod = new Node(counter,head);
+                        newNod.next = newNode;
+                        return newNod;
+
+                }
+
+                public void CreateTailR(int counter){
+                    head = CreateTailRec(counter,head);
+                }
+
+                public Node CreateTailRec(int counter, Node newNode){
+                    if (newNode == null){
+                        return new Node(counter, head);
+                    }
+                    else {
+                        newNode.next = CreateTailRec(counter, newNode.next);
+                        return newNode;
+                    }
+                }
+
+                public String toStringRec() {
+                    return toStringRecHelper(head);
+                }
+
+                private String toStringRecHelper(Node newNode) {
+                    if (newNode == null) {
+                        return "";
+                    }
+                    return newNode.value + " " + toStringRecHelper(newNode.next);
+                }*/
+
+
+
+            public void createHeadRec(int value) {
+                headR = createHeadRecHelper(headR, value);
+            }
+
+            private Rec createHeadRecHelper(Rec curRec, int value) {
+                if (curRec == null) {
+                    return new Rec(value);
+                }
+                Rec newNode = new Rec(value);
+                newNode.next = curRec;
+                return newNode;
+            }
+
+            public void createTailRec(int value) {
+                headR = createTailRecHelper(headR, value);
+            }
+
+            private Rec createTailRecHelper(Rec curRec, int value) {
+                if (curRec == null) {
+                    return new Rec(value);
+                }
+                curRec.next = createTailRecHelper(curRec.next, value);
+                return curRec;
+            }
+
+            public String toStringRec() {
+                return toStringRecHelper(headR);
+            }
+
+            private String toStringRecHelper(Rec curRec) {
+                if (curRec == null) {
+                    return "";
+                }
+                return curRec.value + " " + toStringRecHelper(curRec.next);
+            }
+
 
                  public static void main(String[] args) {
 
                     Cycle headList = new Cycle();
                     Cycle tailList = new Cycle();
-                    Cycle test = new Cycle();
-
-                    switch (1){
+                    Cycle RecList = new Cycle();
+                    Node test1;
+                    Node test2;
+                    Scanner in = new Scanner(System.in);
+                     System.out.println("Цикл - 1; Рекурсия - 2");
+                     int n = in.nextInt();
+                    switch (n){
                         case 1:
                             headList.CreateHead();
-                            test.getNode();
-                            System.out.println(test.toString());
-                            System.out.println(headList.toStringHead());
+                            System.out.println("С головы до изменений: " +headList.toStringHead());
+                            headList.addFirst(1);
+                            headList.removeFirst();
+                            headList.addLast(15);
+                            headList.removeLast();
+                            headList.inputAnyWhere(44,2);
+                            headList.removeAnyWhere(2);
+                            test1= headList.getNode();
+                            System.out.println("С головы после изменений: " + toStringTest(test1));
+                            tailList.CreateTail();
+                            System.out.println("С хвоста до изменений: " +tailList.toStringTail());
+                            tailList.addFirst(1);
+                            tailList.removeFirst();
+                            tailList.addLast(15);
+                            tailList.removeLast();
+                            tailList.inputAnyWhere(44,2);
+                            tailList.removeAnyWhere(2);
+                            test2= tailList.getNode();
+                            System.out.println("С хвоста после изменений: " +toStringTest(test2));
                             break;
                         case 2:
-                            tailList.CreateTail();
-                            System.out.println(tailList.toStringTail());
+                            RecList.createHeadRec(5);
+                            RecList.createTailRec(2);
+                            System.out.println(RecList.toStringRec());
                             break;
                     }
                 }
         }
+    }
     }
 }
